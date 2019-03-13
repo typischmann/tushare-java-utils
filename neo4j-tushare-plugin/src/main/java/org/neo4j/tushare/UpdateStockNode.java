6ac5c42af94c5096ff5a34c2b374eed4eba5e273
baojianyu <baojianyu@xiaomi.com>
2019-03-13 18:30:12 +0800

    refactor the project structure

--- neo4j-tushare-plugin/src/main/java/org/neo4j/tushare/UpdateStockNode.java
+++ neo4j-tushare-plugin/src/main/java/org/neo4j/tushare/UpdateStockNode.java
@@ -1,11 +1,19 @@
 package org.neo4j.tushare;
 
+import com.tushare.bean.ApiResponse;
+import com.tushare.constant.stock.basic.StockBasicFields;
 import com.tushare.core.api.TushareStockDataService;
 import com.tushare.core.impl.DefaultTushareStockDataService;
+import com.tushare.exception.TushareException;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Procedure;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UpdateStockNode {
 
@@ -17,15 +25,35 @@ public class UpdateStockNode {
     // This gives us a log instance that outputs messages to the
     // standard log, normally found under `data/log/console.log`
     @Context
-    public Log log;
+    public Log logger;
 
 
 
     public static final String token = "456b1cb6d086872e59ffd4432c0ef6bc31fa1e6450a3d8b89a1d667d";
 
-    @Procedure
+    @Procedure(value = "tushare.createAndUpdateStockNodes")
     public void createAndUpdateStockNodes(){
         TushareStockDataService tushareStockDataService = new DefaultTushareStockDataService(token);
+        try (Transaction transaction = db.beginTx()){
+
+            ApiResponse apiResponse = tushareStockDataService.stockCompany();
+            Map<String, Integer> fieldMap = new HashMap<>();
+            List<String> fields = apiResponse.getFields();
+            Integer position = 0;
+            for(String field : fields){
+                fieldMap.put(field, position);
+                position++;
+            }
+
+            List<String[]> items = apiResponse.getItems();
+
+            for(String[] item: items){
+                String tsCode = item[fieldMap.get(StockBasicFields.TS_CODE)];
+            }
+            transaction.success();
+        } catch (TushareException e) {
+            logger.error("Tushare Exception : {}", e);
+        }
 
     }
 
---
+++ neo4j-tushare-plugin/src/main/java/org/neo4j/tushare/constant/LabelType.java
@@ -0,0 +1,9 @@
+package org.neo4j.tushare.constant;
+
+public class LabelType {
+
+    public static final String STOCK = "STOCK";
+    public static final String INDUSTRY = "INDUSTRY";
+    public static final String CONCEPT = "CONCEPT";
+
+}
